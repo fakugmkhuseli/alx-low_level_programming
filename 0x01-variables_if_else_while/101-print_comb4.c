93b58ed3bd20f51d13676e861324427d7301303f
fakugmkhuseli <fakugmkhuseli@gmail.com>
2022-06-06 06:40:52 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,31 +4,30 @@
  *
  * Return: 0 (Success)
  */
-int main(void)
+iint main(void)
 {
 	int dig1, dig2, dig3;
 
-	for (dig1 = 0; dig1 < 2; dig1++)
+	for (dig1 = 0; dig1 < 8; dig1++)
 	{
-		for (dig2 = dig1 + 1; dig2 < 3; dig2++)
+		for (dig2i = dig1 + 1; dig2 < 9; dig2++)
 		{
-			for (dig3 = dig2 + 1; dig3 < 4; dig3++)
+			for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 			{
+				putchar((dig1 % 10) + '0');
+				putchar((dig2 % 10) + '0');
+				putchar((dig3 % 10) + '0');
 				
-			putchar((dig1 % 10) + '0');
-			putchar((dig2 % 10) + '0');
-			putchar((dig3 % 10) + '0');
-
-			if (dig1 == 1 && dig2 == 2 && dig3 == 3)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+				if (dig1 == 7 && dig2 == 8 && dig3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-
+	
 	putchar('\n');
-
+	
 	return (0);
 }
