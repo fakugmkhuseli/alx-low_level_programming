c163aa0ec081bf37ac122c48a2f926c7247f56b3
fakugmkhuseli <fakugmkhuseli@gmail.com>
2022-06-07 06:30:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+
+	_putchar('\n');
+
+	return (0);
+}
