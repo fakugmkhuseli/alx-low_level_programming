f2d1c0cf25d74eeacf5d651eceeb9c0c96e5fcf2
fakugmkhuseli <fakugmkhuseli@gmail.com>
2022-06-10 07:17:30 -0700

    Header file containing prototypes for all functions used in the 0x03-more_functions_nested_loops directory.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,8 +6,10 @@
  * Description: This header file contains all prototypes for all functions used in the 0x03-more_functions_nested_loops directory.
  */
 
+int _putchar(char c);
 int _isupper(int c);
-int _isdigit(int c)
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
