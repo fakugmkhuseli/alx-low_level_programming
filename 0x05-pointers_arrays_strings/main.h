50ebf8959790cfd7ad0728f9b87c9e7a774f0a01
fakugmkhuseli <fakugmkhuseli@gmail.com>
2022-06-13 05:20:28 -0700

    Header file containing prototypes for all functions

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Header file name: main.h
+ * Description: This header file contains prototypes of all functions used in the 0x05-pointers_arrays_strings directory.
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
